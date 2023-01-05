1aa4012f3ae3ef7fb76768ee88c595d5b96149d4
Narvit <naravit.a@baraadv.co.th>
2023-01-05 13:04:30 +0700

    V1.0.47

---
+++ database/SQLQuery1.sql
@@ -0,0 +1,16 @@
+	
+	insert into [dbo].[LIMS_BLM]([BATCH_NO],[MATERIAL_CODE],[PRODUCT_TYPE],[PRODUCT_LINE],[PROCESS_STATUS],[CLASSIFY_RESULT],[CLASSIFY_STATUS],[COMMENT],[SHORT_TEXT],[PLAN_PACK],[CREATE_DATE],[UPDATE_DATE])
+	values('60115608', '610257.0000D', 'HP6119', 'PP1', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115616', '610235.0000D', 'HP527NA', 'PP1', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115620', '610010.0000', 'HP400K', 'PP1', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115626', '610034.0000', 'RC213M', 'PP2', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115640', '610010.0000', 'HP400K', 'PP2', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115657', '610015.0000', 'HP525J', 'PP3', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115662', '610015.0000', 'HP525J', 'PP3', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115787', '610039.0000', 'HP562T', 'PP1', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115816', '610009.0000', 'HP400H', 'PP2', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115826', '610212.0000', 'RC221L', 'PP3', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115833', '610169.0000', 'RC221M', 'PP3', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115926', '610183.0000', 'HP748H', 'PP2', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60115993', '610070.0000', 'RP242G', 'PP2', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25'),
+('60116213', '610199.0000', 'RP171G', 'PP3', 'INT', 'Standard', 'WFC', 'Str', 'Str', 'Str', '2022-05-25', '2022-05-25');
